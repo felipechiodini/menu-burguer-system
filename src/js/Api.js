ccf890a027a95a99723aa4fce3147858925cbbd0
Felipe Bona <felipechiodinibona@hotmail.com>
2023-09-13 13:33:11 -0300

    fix

--- src/js/Api.js
+++ src/js/Api.js
@@ -1,10 +1,10 @@
 import Axios from 'axios'
 import Router from '@/router'
 
-const slug = (process.env.NODE_ENV === 'development') ? 'plankton' : window.location.host.split('.')[0]
+const slug = (process.env.NODE_ENV === 'development') ? process.env.VUE_APP_DEVELOPMENT_STORE_SLUG : window.location.host.split('.')[0]
 
 const Api = Axios.create({
-  baseURL: process.env.VUE_APP_API_BASE_URL + '/' + slug + '/delivery',
+  baseURL: `${process.env.VUE_APP_API_BASE_URL}/${slug}`,
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
